fe13ff2f37774a073e28797946a68b440d53fe01
Roman De Rose <romandr37@gmail.com>
2025-03-28 23:34:23 -0300

    Interactividad sección Idols y estilos asociados; integrar React en la configuración de Astro

---
+++ src/components/Idols.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { IDOLS } from "../assets/helpers/idols";
+import "../assets/styles/Idols.css";
+import "../assets/styles/Modal.css";
+
+const ModalIdol = ({ idol, closeModal }) => {
+  const { id, image, name } = idol;
+
+  return (
+    <div className={`modal modal-${id}`} onClick={closeModal}>
+      <i class="bx bx-x"></i>
+      <img
+        src={image}
+        alt={`Foto de ${name}`}
+        decoding="async"
+        loading="lazy"
+        width={300}
+        height={300}
+      />
+      <p>{name.map((n) => n + " ")}</p>
+    </div>
+  );
+};
+
+const Idol = ({ id, image, name, handleIdolClick }) => {
+  return (
+    <article className={`box box-${id}`} onClick={() => handleIdolClick(id)}>
+      <img
+        src={image}
+        alt={`Foto de ${name}`}
+        decoding="async"
+        loading="lazy"
+      />
+
+      <div className="name-idol">
+        <div>
+          {name.map((idolName) => (
+            <p key={idolName}>{idolName}</p>
+          ))}
+        </div>
+      </div>
+    </article>
+  );
+};
+
+function Idols() {
+  const [showIdol, setShowIdol] = useState(false);
+  const [selectedIdol, setSelectedIdol] = useState(null);
+
+  const handleIdolClick = (id) => {
+    const idolSelected = IDOLS.find((idol) => idol.id === id);
+    setSelectedIdol(idolSelected);
+    setShowIdol(true);
+  };
+
+  const handleCloseModal = (e) => {
+    if (e.target.nodeName === "IMG") return;
+
+    setShowIdol(false);
+  };
+
+  return (
+    <section id="idols" className="section-idols">
+      <h2>Idolos</h2>
+      <div className="container-gallery">
+        {IDOLS.map(({ id, image, name }) => (
+          <Idol
+            key={id}
+            id={id}
+            image={image}
+            name={name}
+            handleIdolClick={handleIdolClick}
+          />
+        ))}
+      </div>
+      {showIdol && (
+        <ModalIdol idol={selectedIdol} closeModal={handleCloseModal} />
+      )}
+    </section>
+  );
+}
+
+export default Idols;
