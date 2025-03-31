8805a2ffa2c1f2c0851b52b18bcce9d3d9afa95f
Roman De Rose <92501097+RomaanDeRose@users.noreply.github.com>
2025-03-31 15:28:31 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/static';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  adapter: vercel({ imageService: true })
+  integrations: [react()]
 });
